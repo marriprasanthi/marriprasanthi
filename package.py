deae017710d0b122f3bf56b07cf27abc6b5088eb
marriprasanthi <prasanthi0257@gmail.com>
2025-03-05 12:13:06 +0530

    Add files via upload

---
+++ package.py
@@ -0,0 +1,31 @@
+# Defining a package-like structure inside a single file
+
+# ClassOne Definition
+class ClassOne:
+    def __init__(self):
+        print("ClassOne Constructor Called")
+
+    def display(self):
+        print("Method from ClassOne")
+
+# ClassTwo Definition
+class ClassTwo:
+    def __init__(self):
+        print("ClassTwo Constructor Called")
+
+    def show(self):
+        print("Method from ClassTwo")
+
+mypackage = {
+    "ClassOne": ClassOne,
+    "ClassTwo": ClassTwo
+}
+ClassOneImported = mypackage["ClassOne"]
+ClassTwoImported = mypackage["ClassTwo"]
+
+
+obj1 = ClassOneImported()
+obj1.display()
+
+obj2 = ClassTwoImported()
+obj2.show()
