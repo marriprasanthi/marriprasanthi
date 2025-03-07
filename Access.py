71a49ff8dca3d978c95a91958759c3a011327417
marriprasanthi <prasanthi0257@gmail.com>
2025-03-07 20:36:10 +0530

    Update Access.py

--- Access.py
+++ Access.py
@@ -15,11 +15,11 @@ print(obj.static_variable)
 
 
 class MyClass:
-    # Static variable (also called a class variable)
+
     static_variable = 100
 
     def _init_(self, value):
-        # Instance variable
+       
         self.instance_variable = value
 
     def show_values(self):
@@ -36,19 +36,18 @@ obj1.static_variable = 200
 
 
 class MyClass:
-    # Static variable (class variable)
+
     static_variable = 100
 
     def _init_(self, value):
-        # Instance variable
+    
         self.instance_variable = value
 
     def change_static_variable(self, new_value):
-        # Changing the static variable using the class name (affects all instances)
+    
         MyClass.static_variable = new_value
 
     def change_instance_variable(self, new_value):
-        # This creates an instance variable (only for the current instance)
         self.static_variable = new_value
 
     def show_values(self):
@@ -71,16 +70,16 @@ obj2.show_values()
 
 
 class MyClass:
-    # Static variable (class variable)
+    
     static_variable = 100
 
     def _init_(self, value):
-        # Instance variable
+        
         self.instance_variable = value
 
     @classmethod
     def change_static_variable(cls, new_value):
-        # Changing the static variable using the class method
+        
         cls.static_variable = new_value
 
     def show_values(self):
